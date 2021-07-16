9f0f19ca2b609ea50f1df5e61031d5ecde3a1fec
mohammad rahmati <soft2fire@gmail.com>
2021-07-16 19:08:12 +0430

    remove bug

--- src/Component/CartContext.js
+++ src/Component/CartContext.js
@@ -5,32 +5,32 @@ export const CartContext = createContext();
 export const CartProvider = (props) => {
     const [items, setItems] = useState([]);
 
-    const handleAddInCart = (item, quantity) => {
+    // const handleAddInCart = (item, quantity) => {
 
-        if (items.some(element => element.item.id === item.id)) {
+    //     if (items.some(element => element.item.id === item.id)) {
 
-            items.forEach(element => {
+    //         items.forEach(element => {
 
-                if (element.item.id === item.id) {
-                    const newCartItems = [...items];
-                    const index = newCartItems.indexOf(element);
-                    newCartItems[index].amount = Number(element.amount) + Number(quantity);
-                    setItems(newCartItems);
-                }
+    //             if (element.item.id === item.id) {
+    //                 const newCartItems = [...items];
+    //                 const index = newCartItems.indexOf(element);
+    //                 newCartItems[index].amount = Number(element.amount) + Number(quantity);
+    //                 setItems(newCartItems);
+    //             }
 
-            });
+    //         });
 
-            return;
-        }
+    //         return;
+    //     }
 
-        const cartItem = {
-            item: item,
-            amount: quantity
-        }
-        setItems([...items, cartItem]);
-    }
+    //     const cartItem = {
+    //         item: item,
+    //         amount: quantity
+    //     }
+    //     setItems([...items, cartItem]);
+    // }
 
-    const [cart, setCart] = useState([]);
+    // const [cart, setCart] = useState([]);
     return (
         <CartContext.Provider value={[items, setItems]}  >
             {props.children}
